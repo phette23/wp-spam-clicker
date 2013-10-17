31be7bdb38c72b4b5982655d515389c3e0815c8c
Eric Phetteplace <phette23@gmail.com>
2013-10-17 17:31:54 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+// Generated on 2013-10-16 using generator-bookmarklet 0.1.2
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'main.js'
+      ]
+    },
+
+    uglify: {
+      options: {
+        banner: '// Copy this to your URL bar:\njavascript:',
+        report: 'min'
+      },
+      dist: {
+        files: {
+          'bookmarklet.min.js': [ 'main.js' ]
+        }
+      }
+    },
+
+    watch: {
+      dist: {
+        files: 'main.js',
+        tasks: [ 'uglify' ]
+      }
+    }
+  });
+
+  grunt.registerTask( 'build', [ 'uglify' ] );
+
+  grunt.registerTask( 'server', [ 'watch' ] );
+
+  grunt.registerTask( 'default', [ 'jshint', 'build' ] );
+};
---
+++ main.js
@@ -0,0 +1,130 @@
+/*jshint strict:false, browser:true */
+(function ( undefined ){
+  var d = document,
+    unsupported = !( d.querySelectorAll && d.querySelector && Array.prototype.forEach && Element.prototype.querySelector && Element.prototype.setAttribute ),
+    names = sites = emails = ips = [],
+    totalComments = actedUpon = 0;
+
+  // shortcuts
+  function qa ( sel ) {
+    return d.querySelectorAll( sel );
+  }
+  Element.prototype.qs = Element.prototype.querySelector;
+  Element.prototype.attr = Element.prototype.setAttribute;
+
+  // browser doesn't have necessary DOM methods
+  if ( unsupported ) {
+    console.log( 'Sorry, your browser doesn\'t have the needed features to run this script. Try an up-to-date version of Chrome or Firefox.' );
+    return;
+  }
+
+  // arrays of regexs which comments are tested against
+  names = [ /real estate/
+    , /oakley/i
+    , /sunglasses/i
+    , /massage/i
+    , /mulberry /i
+    , /nike/i
+    , /nuclear power/i
+    , /mortgage/i
+    , /calvin klein/i
+    , /ugg/i
+    , /viagra/i
+    , /phentermine/i
+    , /outlet/i
+    , /cash advance/i
+    , /samsung/i
+    , /ray ban/i
+    , /loans/i
+    , /business/i
+    , /erectile/i
+    , /lasik/i
+    , /valium/i
+    , /klonopin/i
+    , /mitsubishi/i
+    , /vicodin/i
+    , /adderall/i
+    , /percocet/i
+    , /cialis/i
+    , /xanax/i
+    , /ambien/i
+    , /levitra/i
+    , /longchamp/i
+    , /vanessa bruno/i
+    , /the glades/i
+    , /meridia/i
+    , /lorazepam/i
+    , /My Homepage/
+    , /^(URL)$/ ];
+  sites = [ /idealglasses\.net/
+    , /gruppomelearn\.net/
+    , /youtube\.com\/watch\?v/
+    , /hollycalvinklein\.com/
+    , /digital\-import\.co\.uk/
+    , /small\-business\-online\.co\.uk/
+    , /ieeesfsu\.org\.pl/
+    , /iphone2madness\.com/
+    , /paada\.co\.uk/
+    , /abcads\.net/
+    , /clubtickets\.ws/
+    , /eziconex\.com/
+    , /vbro\.co\.uk/
+    , /islamick12\.com/
+    , /removedrmfast\.com/
+    , /mariopireddu\.net/
+    , /hopegfx\.com/
+    , /proxykat\.net/ ];
+  // emails = [ ];
+  // ips = [ ];
+
+  // extend NodeList with Array's forEach method
+  NodeList.prototype.forEach = Array.prototype.forEach;
+
+  // check each comment for matches
+  totalComments = qa( '#the-comment-list tr' ).length;
+  qa( '#the-comment-list tr' ).forEach( function ( val, ind, arr ) {
+    var col = val.qs( '.author' ),
+      acted = false,
+      comment = {
+        name: col.qs( 'strong' ) ? col.qs( 'strong' ).textContent : '',
+        site: col.qs( ' a[title]' ) ? col.qs( ' a[title]' ).href : '',
+        email: col.qs( 'a[href^="mailto"]' ) ? col.qs( 'a[href^="mailto"]' ).href.replace( 'mailto:', '' ) : '',
+        ip: col.qs( 'a[href^="edit-comments.php?s="]' ) ? col.qs( 'a[href^="edit-comments.php?s="]' ).textContent : ''
+      };
+
+    names.forEach( function ( val2, ind2, arr2 ) {
+      if ( comment.name.match( val2 ) ) {
+        val.qs( 'input[type="checkbox"]' ).attr( 'checked', 'true' );
+        acted = true;
+      }
+    });
+
+    sites.forEach( function ( val2, ind2, arr2 ) {
+      if ( comment.site.match( val2 ) ) {
+        val.qs( 'input[type="checkbox"]' ).attr( 'checked', 'true' );
+        acted = true;
+      }
+    });
+
+    emails.forEach( function ( val2, ind2, arr2 ) {
+      if ( comment.email.match( val2 ) ) {
+        val.qs( 'input[type="checkbox"]' ).attr( 'checked', 'true' );
+        acted = true;
+      }
+    });
+
+    ips.forEach( function ( val2, ind2, arr2 ) {
+      if ( comment.ip.match( val2 ) ) {
+        val.qs( 'input[type="checkbox"]' ).attr( 'checked', 'true' );
+        acted = true;
+      }
+    });
+
+    if ( acted ) {
+      actedUpon += 1;
+    }
+
+  });
+
+  console.log( '%d comments selected out of %d', actedUpon, totalComments );
+}());
